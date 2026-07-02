2d24419b9c7b3e4b6b94a1995c400579c6013d7c
alexxanton <alexantonaranda@gmail.com>
2026-07-03 01:11:01 +0200

    updated the file name and added a function to check if the simulation is running

---
+++ src/utils/utils.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   monitoring.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aanton-a <aanton-a@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/25 12:09:37 by aanton-a          #+#    #+#             */
+/*   Updated: 2026/06/25 12:10:08 by aanton-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <codexion.h>
+
+bool	has_finished(t_coder *coder)
+{
+	bool	finished;
+
+	pthread_mutex_lock(&coder->lock);
+	finished = coder->compiles_left <= 0;
+	pthread_mutex_unlock(&coder->lock);
+	return (finished);
+}
+
+bool	simulation_running(t_data *data)
+{
+	bool	run;
+
+	pthread_mutex_lock(&data->burnout_lock);
+	run = data->run;
+	pthread_mutex_unlock(&data->burnout_lock);
+	return run;
+}
