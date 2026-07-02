eb970d8f423894e30565f637f65e10ac3c82006b
Alex Anton Aranda <aanton-a@cbr3s5.42barcelona.com>
2026-07-02 16:50:25 +0200

    implemented proper time simulation for coder tasks

--- src/coders/routine.c
+++ src/coders/routine.c
@@ -6,7 +6,7 @@
 /*   By: aanton-a <aanton-a@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/25 12:08:20 by aanton-a          #+#    #+#             */
-/*   Updated: 2026/07/01 15:28:25 by aanton-a         ###   ########.fr       */
+/*   Updated: 2026/07/02 16:47:21 by aanton-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,16 @@
 static void	simulate_task(t_data *data, long time)
 {
 	bool	run;
+	long	end;
 
+	end = get_time_ms(data) + time;
 	run = true;
-	while (run && time > 0)
+	while (run && get_time_ms(data) < end)
 	{
 		pthread_mutex_lock(&data->burnout_lock);
 		run = data->run;
 		pthread_mutex_unlock(&data->burnout_lock);
-		usleep(1000);
-		time--;
+		usleep(500);
 	}
 }
 
@@ -54,23 +55,36 @@ void	*routine(void *arg)
 	data = self->data;
 	while (self->compiles_left)
 	{
-		take_dongle(self, self->right_dongle);
-		take_dongle(self, self->left_dongle);
+		//pthread_mutex_lock(&data->burnout_lock);
+		//if (!data->run)
+		//	break ;
+		//pthread_mutex_unlock(&data->burnout_lock);
+
+		if (self->id % 2 == 0)
+		{
+			take_dongle(self, self->right_dongle);
+			take_dongle(self, self->left_dongle);
+		}
+		else
+		{
+			take_dongle(self, self->left_dongle);
+			take_dongle(self, self->right_dongle);
+		}
 
 		pthread_mutex_lock(&self->lock);
 		self->last_compile = get_time_ms(self->data);
 		pthread_mutex_unlock(&self->lock);
 
 		execute_task(self, data->time_to_compile);
+		release_dongle(self->left_dongle);
+		release_dongle(self->right_dongle);
+
 		execute_task(self, data->time_to_debug);
 		execute_task(self, data->time_to_refactor);
 
 		pthread_mutex_lock(&self->lock);
 		self->compiles_left--;
 		pthread_mutex_unlock(&self->lock);
-
-		release_dongle(self->left_dongle);
-		release_dongle(self->right_dongle);
 	}
 	return (NULL);
 }
