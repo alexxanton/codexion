c96c61eb72f35e6894617222a9dce1c698425cb0
Alex Anton Aranda <aanton-a@cbr3s5.42barcelona.com>
2026-07-02 16:51:38 +0200

    added cond wait and signal

--- src/dongles/dongle.c
+++ src/dongles/dongle.c
@@ -6,7 +6,7 @@
 /*   By: aanton-a <aanton-a@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/01 14:50:20 by aanton-a          #+#    #+#             */
-/*   Updated: 2026/07/01 15:30:21 by aanton-a         ###   ########.fr       */
+/*   Updated: 2026/07/02 14:44:50 by aanton-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,13 @@
 void	take_dongle(t_coder *coder, t_dongle *dongle)
 {
 	pthread_mutex_lock(&dongle->lock);
-	if (dongle->is_taken)
-	{
-		pthread_mutex_unlock(&dongle->lock);
-		return ;
-	}
+	//if (dongle->is_taken)
+	//{
+	//	pthread_mutex_unlock(&dongle->lock);
+	//	return ;
+	//}
+	while (dongle->is_taken)
+		pthread_cond_wait(&dongle->available, &dongle->lock);
 	dongle->is_taken = true;
 	pthread_mutex_unlock(&dongle->lock);
 	print_log(coder, "has taken a dongle");
@@ -29,5 +31,6 @@ void	release_dongle(t_dongle *dongle)
 {
 	pthread_mutex_lock(&dongle->lock);
 	dongle->is_taken = false;
+	pthread_cond_signal(&dongle->available);
 	pthread_mutex_unlock(&dongle->lock);
 }
